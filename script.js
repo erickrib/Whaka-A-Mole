f2614ee1692d89647a44c4d2201953b143bd619a
Erick Ribeiro <erickrocha965@gmail.com>
2021-12-19 17:45:46 -0300

    four-version

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const principal = document.querySelector(".principal")
 const cabecalho = document.querySelector(".menu")
 const holes = document.querySelectorAll(".hole-img")
 const stopbtn = document.querySelector(".stop-button")
+const returnbtn = document.querySelector(".return")
+const finishbtn = document.querySelector(".finish")
+const score = document.querySelector(".numbers")
 
 addEventListener("load", () => setTimeout(() =>
     mode.style.display = "block", 500))
@@ -13,6 +16,12 @@ start.addEventListener("click", () => {
     principal.style.backgroundBlendMode = "initial"
     cursor.style.display = "block"
     stopbtn.style.display = "block"
+
+    var duration = 60 * 2
+    var display = document.querySelector(".timer-number")
+
+    startTimer(duration, display)
+
     addEventListener("mousemove", (e) => {
         cursor.style.top = e.pageY + "px"
         cursor.style.left = e.pageX + "px"
@@ -22,17 +31,19 @@ start.addEventListener("click", () => {
         principal.addEventListener("mouseleave", () => {
             cursor.style.display = "none"
         })
+
         addEventListener("click", () => {
             cursor.style.animation = "hit 0.1s ease"
             setTimeout(() => {
                 cursor.style.removeProperty("animation")
-              }, 80)
-          })
-      })
+            }, 80)
+        })
+    })
 
     let hole
     let points = 0
-    const startGame = setInterval(() => {
+
+    const startGame = setInterval(function startt() {
         let arrayNo = Math.floor(Math.random() * 9)
         hole = holes[arrayNo]
 
@@ -43,19 +54,72 @@ start.addEventListener("click", () => {
 
         setTimeout(() => {
             hole.removeChild(image)
-        }
-        ,1900)
+        }, 1900)
+
+        stopbtn.addEventListener("mouseover", () => cursor.style.display = "none")
+        stopbtn.addEventListener("click", () => {
+            for(let i=0; i< holes.length;i++){
+               holes[i].style.width = "0";
+               holes[i].style.height = "0";
+            }
+            stopbtn.style.display = "none"
+            returnbtn.style.display = "block"
+            finishbtn.style.display = "block"
+        })
+
+        returnbtn.addEventListener("click", () => {
+            for(let i=0; i< holes.length;i++){
+                holes[i].style.width = "90px";
+                holes[i].style.height = "110px";
+             }
+            stopbtn.style.display = "block"
+            returnbtn.style.display = "none"
+            finishbtn.style.display = "none"
+        })
+
+        finishbtn.addEventListener("click", () => {
+            score.innerText = 0
+        })
+
     }, 2000)
-    let score = document.querySelector(".numbers")
+
     addEventListener("click", (e) => {
         if (e.target === hole) {
-        score.innerText = ++points
+            score.innerText = ++points
         }
+    })
+
+    function startTimer(duration, display) {
+        display.style.display = "block"
+        let timer = duration,
+            minutes, seconds
+
+        const timercont = setInterval(function () {
+            minutes = parseInt(timer / 60, 10)
+            seconds = parseInt(timer % 60, 10)
+
+            minutes = minutes < 10 ? "0" + minutes : minutes
+            seconds = seconds < 10 ? "0" + seconds : seconds
+
+            display.innerText = minutes + ":" + seconds
+
+            if (--timer < -1) {
+                timer = duration
+            }
+            if (timer == -1) {
+
+                clearInterval(startGame)
+                clearInterval(timercont)
+                score.innerText = 0
+            }
+
+        }, 1000);
+        finishbtn.addEventListener("click", () => {
+            clearInterval(timercont)
+            display.style.display = "none"
         })
-    stopbtn.addEventListener("mouseover", () => cursor.style.display = "none")
-    stopbtn.addEventListener("click", () => clearInterval(startGame)
-    )
+    }
+
+
+
 })
-
-
-
